2d0b2866e243ae2c0edfd58cc9d364430a3b6b26
Piotr Rekos <altair.1999@wp.pl>
2021-05-15 18:29:21 +0200

    Make layout of Home Page
    -Made: background,svg elements , styles of the site
    -to Make : Sticky side menu, contentindicators that changes color on click

---
+++ controllers/index-1.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+exports.home = (req,res)=>{
+    var svg = new Array(6);
+    for(var i=1;i<=6;i++)
+        {
+            svg[i] = fs.readFileSync(`\public/homepage-images/svg/${i}.svg`,{encoding:'utf8'}); //encoding is neccesary
+            console.log(svg[i]);
+        }
+    
+    res.render("home",{svg:svg});
+}
--- routers/router.js
+++ routers/router.js
@@ -4,7 +4,7 @@ const controller1 = require("../controllers/index-1.js");
 const Router = express.Router();
 
 
-
+Router.get("/",controller1.home);
 
 
 module.exports = Router;
